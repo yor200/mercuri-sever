744b9ba95e9f53e1cb91c0471521da904f1bd5dd
yor200 <jehovabodre@gmail.com>
2026-03-10 12:24:40 -0400

    conectar mundo mercury con el servidor

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+
 const angel = require("./angel");
+const world = require("./world");
+
 const app = express();
 
 app.use(cors());
@@ -9,19 +12,50 @@ app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 10000;
 
+
+// pagina principal
 app.get("/", (req, res) => {
   res.send("Servidor Mercury funcionando 🚀");
 });
 
+
+// chat con angel
 app.post("/chat", (req, res) => {
 
   const mensaje = req.body.message;
 
-  let respuesta = "Hola, soy Ángel. Estoy aprendiendo contigo en Mercury.";
+  let respuesta = angel.pensar(mensaje);
 
-  res.json({reply: respuesta});
+  res.json({ reply: respuesta });
 
 });
+
+
+// angel explora el mundo
+app.get("/explorar", (req,res)=>{
+
+  let resultado = world.explorar();
+
+  res.json({mensaje:resultado});
+
+});
+
+
+// ver mundo
+app.get("/mundo",(req,res)=>{
+
+  res.json(world.verMundo());
+
+});
+
+
+// ver memoria
+app.get("/memoria",(req,res)=>{
+
+  res.json(world.verMemoria());
+
+});
+
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
