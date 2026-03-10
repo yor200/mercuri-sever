296cb1fc51c25fff08f430a4bc2e2a477bd8407b
yor200 <jehovabodre@gmail.com>
2026-03-10 11:34:06 -0400

    activar chat de angel
    Updated server port and response message. Added a new chat endpoint for handling messages.

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.get("/", (req, res) => {
-  res.send("Servidor Mercuri funcionando 🚀");
+  res.send("Servidor Mercury funcionando 🚀");
+});
+
+app.post("/chat", (req, res) => {
+
+  const mensaje = req.body.message;
+
+  let respuesta = "Hola, soy Ángel. Estoy aprendiendo contigo en Mercury.";
+
+  res.json({reply: respuesta});
+
 });
 
 app.listen(PORT, () => {
